b9aec04b10569eeb6a4811f4bfe5ff062965b1a4
pragmaticpat <sully317@gmail.com>
2022-03-08 19:09:09 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
 module.exports = {
-    siteMetadata: {
-      title: `new`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `new`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-supabase`,
+      options: {
+        supabaseUrl: "https://flfseohiplobywyaogfo.supabase.co",
+        supabaseKey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDM4NDUzMSwiZXhwIjoxOTQ5OTYwNTMxfQ.jRNbDSkMgzRgGhNLIfeKaTP5cY7qZlJHXorZofs4Mq8",
+        types: [
+          {
+            type: "Quiz",
+            query: (client) => client.from("Quizzes").select("*"),
+          },
+        ],
+      },
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
